19d6e03b0ce5d89d7fb5dec7be695c5cb92b932c
Theju Prasad <tthejuprasad00@gmail.com>
2023-09-25 11:17:50 +0530

    change

--- Server.js
+++ Server.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost/job_search_app', { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-=======
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -33,5 +15,5 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-const jobRoutes=require('./routes/jobRoutes');
-app.use('/api/jobs',jobRoutes);
+const jobRoutes = require('./routes/jobRoutes');
+app.use('/api/jobs', jobRoutes);

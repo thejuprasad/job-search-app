417a5bf16f367ea3008760a562f51cccee0a8022
Theju Prasad <tthejuprasad00@gmail.com>
2023-09-25 10:55:45 +0530

    create

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/job_search_app', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
